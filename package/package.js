62f9dd52f742b3098c1e0713569aaf8404380820
xamfoo <xamfoo@all.repo>
2015-05-05 13:15:58 +0800

    Fix broken repo link

--- package/package.js
+++ package/package.js
@@ -4,7 +4,7 @@ Package.describe({
   // Brief, one-line summary of the package.
   summary: "Blaze add-on: Create stateful components with methods and mixins like in Facebook's React",
   // URL to the Git repository containing the source code for this package.
-  git: 'https://github.com/xamfoo/meteor-blazer-react',
+  git: 'https://github.com/xamfoo/blazer-react',
   // By default, Meteor will default to using README.md for documentation.
   // To avoid submitting documentation, set this field to null.
   documentation: 'README.md'
